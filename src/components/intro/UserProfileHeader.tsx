42db7b0633b4abdc58d4b01a3dcfa6304bb2bb09
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-11-27 20:27:32 -0500

    Remove api call from profile component

--- src/components/intro/UserProfileHeader.tsx
+++ src/components/intro/UserProfileHeader.tsx
@@ -2,52 +2,43 @@
 
 import React from 'react'
 import styles from './UserProfileIntro.module.css';
-import { useUserProfile } from "@/context/ProfileContext";
-import UserProfileHeaderPlaceholder from './UserProfileHeaderPlaceholder';
+import { UserProfile, useUserProfile } from "@/context/ProfileContext";
 import GitHubIcon40x40 from '@/components/icons/GitHubIcon40x40'
 import LinkedinIcon40x40 from '@/components/icons/LinkedinIcon40x40'
 
-const UserProfileHeader = () => {
+interface UserProfileProps {
+    profileData: UserProfile | null;
+}
 
-    const { userProfile, loading } = useUserProfile();
-    if (!loading) {
-        console.log(userProfile);
-    }
-
-    if (loading) {
-        return <UserProfileHeaderPlaceholder />;
-    }
-
+const UserProfileHeader: React.FC<UserProfileProps> = ({ profileData }) => {
     return (
-        <div>
-            <div className='card shadow-none p-2 mb-5 mt-1 bg-body-tertiary rounded'>
-                <div className={`${styles.profileHeaderRow} card-body`}>
-                    <div className={styles.profileImageWrapper}>
-                        <img src={userProfile?.image ?? "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="} alt='user profile image' />
+        <div className='card shadow-none p-2 mb-5 mt-1 bg-body-tertiary rounded'>
+            <div className={`${styles.profileHeaderRow} card-body`}>
+                <div className={styles.profileImageWrapper}>
+                    <img src={profileData?.image ?? "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="} alt='user profile image' />
+                </div>
+                <div>
+                    <div className={styles.textCenterMobileView}>
+                        <p className='fw-bold fs-3 mb-0 pb-0'>{profileData?.name}</p>
+                        <p className='fw-bold fs-7 text-primary'>{profileData?.role}</p>
                     </div>
-                    <div>
-                        <div className={styles.textCenterMobileView}>
-                            <p className='fw-bold fs-3 mb-0 pb-0'>{userProfile?.name}</p>
-                            <p className='fw-bold fs-7 text-primary'>{userProfile?.role}</p>
-                        </div>
-                        <p>{userProfile?.about}</p>
-                        <div className={styles.socialIconsCenterMobileView}>
-                            <a
-                                className='me-3'
-                                href={userProfile?.github}
-                                target="_blank"
-                                title={`Visit ${userProfile?.name}'s GitHub profile`}
-                            >
-                                <GitHubIcon40x40 />
-                            </a>
-                            <a
-                                href={userProfile?.linkedin}
-                                target="_blank"
-                                title={`View ${userProfile?.name}'s LinkedIn profile`}
-                            >
-                                <LinkedinIcon40x40 />
-                            </a>
-                        </div>
+                    <p>{profileData?.about}</p>
+                    <div className={styles.socialIconsCenterMobileView}>
+                        <a
+                            className='me-3'
+                            href={profileData?.github}
+                            target="_blank"
+                            title={`Visit ${profileData?.name}'s GitHub profile`}
+                        >
+                            <GitHubIcon40x40 />
+                        </a>
+                        <a
+                            href={profileData?.linkedin}
+                            target="_blank"
+                            title={`View ${profileData?.name}'s LinkedIn profile`}
+                        >
+                            <LinkedinIcon40x40 />
+                        </a>
                     </div>
                 </div>
             </div>
--- src/components/myProfile.tsx
+++ src/components/myProfile.tsx
@@ -1,18 +1,24 @@
-'use client'
+'use client';
 
 import UserProfileHeader from "./intro/UserProfileHeader";
+import UserProfileHeaderPlaceholder from "./intro/UserProfileHeaderPlaceholder";
 import UserSkill from "./skill/UserSkill";
 import { useUserProfile } from "@/context/ProfileContext";
 
 export default function MyProfile() {
-
     const { userProfile, loading } = useUserProfile();
     const skills = userProfile?.skills || [];
+
     return (
         <main className="container">
-            <UserProfileHeader/>
-            <UserSkill skills={skills}/>
+            {loading ? (
+                <UserProfileHeaderPlaceholder />
+            ) : (
+                <>
+                    <UserProfileHeader profileData={userProfile} />
+                    <UserSkill skills={skills} />
+                </>
+            )}
         </main>
     );
 }
-
