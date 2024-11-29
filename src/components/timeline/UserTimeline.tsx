fedab7419d4d304442455512c3406cea908f371d
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-11-29 00:40:47 -0500

    Display timelne only in mobile

--- src/components/timeline/UserTimeline.tsx
+++ src/components/timeline/UserTimeline.tsx
@@ -1,37 +1,52 @@
 import React from 'react';
 import styles from './UserTimeline.module.css';
 import { EmploymentHistory } from '@/context/ProfileContext';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 interface EmploymentHistoryProps {
   employmentHistory: EmploymentHistory[] | undefined;
 }
 
 const UserTimeline: React.FC<EmploymentHistoryProps> = ({ employmentHistory }) => {
+  const { isMobile } = useMediaQuery();
 
-  return (
+  const renderTimelineCardMobileView = (item: EmploymentHistory, index: number) => {
+    const isEven = index % 2 === 0;
+    return (
+      <div
+        className={`${styles.card} ${isEven ? styles.even : styles.odd}`}
+        key={index}
+      >
+        <div className={styles.info}>
+          <h4 className={styles.title}>{item.employer.companyName}</h4>
+          <p className={`${isEven ? '' : 'text-end'} fw-bold mb-0 pb-0`}>{item.jobTitle}</p>
+          <p className={`${isEven ? '' : 'text-end'} fw-bold mb-0 pb-0`}>{item.employmentPeriod} ({item.employmentType})</p>
+          <p className={`${isEven ? '' : 'text-end'}`}>{item.jobLocation}</p>
+          <ul>
+            {item.jobResponsibilities.map((responsibility: any, i) => {
+              return <li key={i} className='lh-lg'>{responsibility}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const timelineContentMobileView = (
     <div className={styles.timeline}>
       <div className={styles.outer}>
-        {employmentHistory?.map((item, index) => (
-          <div
-            className={`${styles.card} ${index % 2 === 0 ? styles.even : styles.odd}`}
-            key={index}
-          >
-            <div className={styles.info}>
-              <h4 className={styles.title}>{item.employer.companyName}</h4>
-              <p className={`${index % 2 === 0 ? '' : 'text-end'} fw-bold mb-0 pb-0`}>{item.jobTitle}</p>
-              <p className={`${index % 2 === 0 ? '' : 'text-end'} fw-bold mb-0 pb-0`}>{item.employmentPeriod} ({item.employmentType})</p>
-              <p className={`${index % 2 === 0 ? '' : 'text-end'}`}>{item.jobLocation}</p>
-              <ul>
-                {item.jobResponsibilities.map((responsibility: any, i) => {
-                  return <li key={i} className='lh-lg'>{responsibility}</li>;
-                })}
-              </ul>
-            </div>
-          </div>
-        ))}
+        {employmentHistory?.map((item, index) => renderTimelineCardMobileView(item, index))}
       </div>
     </div>
   );
+
+  const timelineContentTabletDestop = (
+    <div>
+      
+    </div>
+  );
+
+  return isMobile ? timelineContentMobileView : timelineContentTabletDestop;
 };
 
 export default UserTimeline;
