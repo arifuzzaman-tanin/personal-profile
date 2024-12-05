bb5e71eb8e8be1f0c1aec07d12abfac7ae1f2a31
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-12-04 23:23:16 -0500

    Add meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
 import MyProfile from "@/components/myProfile";
+import { generatePageMetadata } from "@/utils/generateMetadata";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const metadata = await generatePageMetadata('', true);
+
+  return metadata;
+}
 
 export default function Home() {
   return (
